50c4f36f4d8495c3a562508c6edf8c2747776400
agreco <greco031@umn.edu>
2023-02-10 17:24:16 -0600

    changed glimpse() to str() as str() explicitly identifies tibble structure in output

--- R/week4.R
+++ R/week4.R
@@ -4,5 +4,6 @@ library("tidyverse")
 
 # Data Import
 import_tbl <- read_delim(file ="../data/week4.dat", col_names = c("casenum", "parnum", "stimver", "datadate",  "qs"), delim = "-")
-glimpse(import_tbl)
+str(import_tbl)
 wide_tbl <- import_tbl %>% separate("qs", sep = " - ", into = c("q1","q2","q3","q4", "q5"), convert = TRUE)
+str(wide_tbl)
