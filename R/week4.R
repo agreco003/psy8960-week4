8584a8b71e941c4d1aa8505aa26a10d9eaca7f6b
agreco <greco031@umn.edu>
2023-02-10 18:09:14 -0600

    switched back to glimpse() because it is an explicit  tidy function. Converted time column to a time format

--- R/week4.R
+++ R/week4.R
@@ -4,6 +4,7 @@ library("tidyverse")
 
 # Data Import
 import_tbl <- read_delim(file ="../data/week4.dat", col_names = c("casenum", "parnum", "stimver", "datadate",  "qs"), delim = "-")
-str(import_tbl)
-wide_tbl <- import_tbl %>% separate("qs", sep = " - ", into = c("q1","q2","q3","q4", "q5"), convert = TRUE)
-str(wide_tbl)
+glimpse(import_tbl)
+wide_tbl <- import_tbl %>% separate("qs", sep = " - ", into = c("q1","q2","q3","q4","q5"), convert = TRUE)
+sapply(wide_tbl[,c("q1","q2","q3","q4","q5")], as.integer)
+wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = c("%b %d %Y, %H:%M:%S"))
